d021b9ef2eb0189e06ee9bc58c3e1b6c27eb689c
tkaos <juan.ticlia@utec.edu.pe>
2025-11-16 16:20:34 -0500

    login funcional

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/index.css'
-import App from './App.tsx'
+import { AuthProvider } from "./contexts/AuthContext";
+import { router } from "./router/routes";
+import "./styles/index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<AuthProvider>
+			<RouterProvider router={router} />
+		</AuthProvider>
+	</StrictMode>,
+);
